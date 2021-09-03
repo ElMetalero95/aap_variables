ff3a9236c6b588c099d995e852527003f40950d3
ElMetalero95 <facundopaez@live.com.ar>
2021-09-03 18:56:58 -0300

    ejercicio de if y condicionales

--- main.py
+++ main.py
@@ -4,10 +4,47 @@
 #print(saludo + nombre)
 
 
-num1 = int(input("Ingrese un numero "))
-num2 = int(input("Ingrese otro numero "))
+ #num1 = int(input("Ingrese un numero "))
+ #num2 = int(input("Ingrese otro numero "))
 
-print(num1 + num2)
+"""print(num1 + num2)
 print(num1 - num2)
 print(num1 * num2)
 print(num1 / num2)
+"""
+
+"""
+if y Operadores de comparacion
+"""
+
+#print(5 < 10) #True
+#print(2 > 20) #False
+#print(6 <= 6) #True
+#print(0 == 0) #True
+"""
+prom = input("pedir promedio")
+si prom > 6 entonces
+  aprueba
+sino
+  rerprueba
+"""
+
+#prom = int (input("pedir promedio "))
+
+#if (prom > 6):
+	#print("aprobado")
+#else:
+	#print("desaprobado")
+
+
+nota1 = int (input("ingrese primera nota ")) 
+nota2 = int (input("ingrese segunda nota ")) 
+nota3 = int (input("ingrese tercera nota "))
+prom = (nota1 + nota2 + nota3)/3
+
+print ("El promedio es: %.2f" % prom)
+
+if (prom > 6):
+	 print("aprobado")
+else:
+	 print("desaprobado")
