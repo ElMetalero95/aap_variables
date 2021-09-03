5eb17ff7ce65dc9a9df1942f4610a9d6a7085f4c
ElMetalero95 <facundopaez@live.com.ar>
2021-09-03 16:54:03 -0300

    first commint

---
+++ main.py
@@ -0,0 +1,13 @@
+#saludo = "Hola "
+#nombre = input ("Ingrese su Nombre")
+
+#print(saludo + nombre)
+
+
+num1 = int(input("Ingrese un numero "))
+num2 = int(input("Ingrese otro numero "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
